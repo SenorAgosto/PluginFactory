7b57dd4f66bf22c474bda45e352fe53e83c3d8a5
Austin Gilbert <ceretullis@gmail.com>
2015-10-14 19:07:33 -0500

    Adding Boost::filesystem and Boost::System as dependencies.

--- _cmake/dependencies.cmake
+++ _cmake/dependencies.cmake
@@ -2,4 +2,7 @@
 include(_cmake/AddLibrary/Dependencies.cmake)
 
 # external libraries we're going to use. 
+include(_cmake/dependencies/Boost.cmake)
+
+# external libraries used for testing. 
 include(_cmake/dependencies/UnitTest++.cmake)
