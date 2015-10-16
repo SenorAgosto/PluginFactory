8b55e46bb04cbee262193b0c9e6de3fc37478a61
Austin Gilbert <ceretullis@gmail.com>
2015-10-16 15:36:22 -0500

    Switching code to use build_traits::platform.

--- PluginFactory/details/PluginLoader.hpp
+++ PluginFactory/details/PluginLoader.hpp
@@ -7,16 +7,14 @@
 // include specialization for windows
 #ifdef WIN32
     #include <PluginFactory/platform/win32/PluginLoaderImpl.hpp>
-    #define PluginLoaderPlatform build_traits::Platform::windows
 #endif
 
 // include specialization for posix
 #ifdef POSIX
     #include <PluginFactory/platform/posix/PluginLoaderImpl.hpp>
-    #define PluginLoaderPlatform build_traits::Platform::posix
 #endif
 
 namespace PluginFactory { namespace details {
     
-    using PluginLoader = PluginLoaderImpl<PluginLoaderPlatform>;
+    using PluginLoader = PluginLoaderImpl<build_traits::platform>;
 }}
