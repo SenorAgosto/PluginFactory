1157783f358b883f026e8927036f5c8c1b32fc7b
Austin Gilbert <ceretullis@gmail.com>
2015-10-16 15:35:34 -0500

    Adding a value 'platform' so we don't need to use macro defined values for platform.

--- PluginFactory/details/build_traits.hpp
+++ PluginFactory/details/build_traits.hpp
@@ -1,9 +1,14 @@
 #pragma once 
 
+#define BUILD_TRAITS_PLATFORM Platform::macosx
+
 struct build_traits
 {
     enum class Platform {
-          windows
+          macosx
+        , windows
         , posix
     };
+    
+    static const Platform platform = BUILD_TRAITS_PLATFORM;
 };
