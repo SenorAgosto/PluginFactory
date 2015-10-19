4ede061c945bbf0bc36f16088a0dca514d9aeb43
Austin Gilbert <ceretullis@gmail.com>
2015-10-19 16:35:20 -0500

    comment: documenting the header layout.

--- PluginFactory/details/PluginLoader.hpp
+++ PluginFactory/details/PluginLoader.hpp
@@ -1,6 +1,16 @@
 #pragma once 
 #include <PluginFactory/details/build_traits.hpp>
 
+////////////////////////////////////
+// This header is organized in the following way:
+// - The primary template declaration is included first.
+// - every platform's template specializations are included next.
+//
+// This arrangement allows us to avoid an over abundance
+// of macro logic. All of the macro logic is contained
+// here or in build_traits. Everything else is
+// normal C++ code.
+
 // include the primary template.
 #include <PluginFactory/details/PluginLoaderImpl.hpp>
 
