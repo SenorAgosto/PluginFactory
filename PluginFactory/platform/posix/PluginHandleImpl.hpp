a979afff4cee23af9888a74a00d15259acfb62ab
Austin Gilbert <ceretullis@gmail.com>
2015-10-21 18:52:53 -0500

    Adding some better comments about the namespace in some POSIX specializations.

--- PluginFactory/platform/posix/PluginHandleImpl.hpp
+++ PluginFactory/platform/posix/PluginHandleImpl.hpp
@@ -1,10 +1,20 @@
 #pragma once 
 #include <PluginFactory/platform/posix/PosixPluginHandle.hpp>
 
+// the namespace here doesn't match the filesystem layout because
+// these are POSIX specializations of PluginFactory::details::PluginHandleImpl
 namespace PluginFactory { namespace details {
 
     template<class PluginInterface, class PluginServiceInterface>
     struct PluginHandleImpl<PluginInterface, PluginServiceInterface, build_traits::Platform::macosx>
+        : public platform::posix::PosixPluginHandle<PluginInterface, PluginServiceInterface>
+    {
+        // pull in constructors from base.
+        using platform::posix::PosixPluginHandle<PluginInterface, PluginServiceInterface>::PosixPluginHandle;
+    };
+    
+    template<class PluginInterface, class PluginServiceInterface>
+    struct PluginHandleImpl<PluginInterface, PluginServiceInterface, build_traits::Platform::linux>
         : public platform::posix::PosixPluginHandle<PluginInterface, PluginServiceInterface>
     {
         // pull in constructors from base.
--- PluginFactory/platform/posix/PluginLoaderImpl.hpp
+++ PluginFactory/platform/posix/PluginLoaderImpl.hpp
@@ -2,6 +2,8 @@
 #include <PluginFactory/details/build_traits.hpp>
 #include <PluginFactory/platform/posix/PosixPluginLoader.hpp>
 
+// the namespace here doesn't match the filesystem layout because
+// these are POSIX specializations of PluginFactory::details::PluginLoaderImpl
 namespace PluginFactory { namespace details {
     
     // Mac OS X specialization
