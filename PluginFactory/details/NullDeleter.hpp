c63106acb30980e4e37843f8a5f8a6c720433748
Austin Gilbert <ceretullis@gmail.com>
2015-10-21 17:50:01 -0500

    Removing PluginDeleter, we're going to keep memory allocated in the shared library over in the shared library... crossing the boundary was a bad bad idea, I was warned back in the day :)

--- PluginFactory/details/NullDeleter.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once 
-
-namespace PluginFactory { namespace details {
-
-    // a function with proper signature to be used by PluginDeleter,
-    // but that doesn't do anything.
-    void null_deleter(void*);
-
-}}
--- PluginFactory/src/details/NullDeleter.cpp
+++
@@ -1,6 +0,0 @@
-#include <PluginFactory/details/Nulldeleter.hpp>
-
-namespace PluginFactory { namespace details {
-
-    void null_deleter(void*){}
-}}
