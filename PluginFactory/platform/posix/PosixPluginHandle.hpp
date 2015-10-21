fbbb1b263d7a8689ecafb1133431fc625f0dac79
Austin Gilbert <ceretullis@gmail.com>
2015-10-21 18:09:30 -0500

    Changes to PosixPluginHandle and PosixPluginLoader - removed loading of deletePlugin() method from plugin shared library. - removed code for returning std::unique_ptr and std::shared_ptr handles of plugins, raw pointers only will be returned.

--- PluginFactory/platform/posix/PosixPluginHandle.hpp
+++ PluginFactory/platform/posix/PosixPluginHandle.hpp
@@ -1,5 +1,4 @@
-#pragma once 
-#include <PluginFactory/PluginDeleter.hpp>
+#pragma once
 #include <PluginFactory/platform/posix/LibraryHandle.hpp>
 
 namespace PluginFactory { namespace platform { namespace posix {
@@ -9,49 +8,30 @@ namespace PluginFactory { namespace platform { namespace posix {
     {
     public:
         using CreatePluginMethod = void* (*)(void*);
-        using DeletePluginMethod = void (*)(void*);
-        
-        struct AsSharedPtr {} static create_shared;
-        
-        PosixPluginHandle(LibraryHandle libraryHandle, void* createAddress, void* deleteAddress);
+        PosixPluginHandle(LibraryHandle libraryHandle, void* createAddress);
 
         // invoke the createPlugin method contained in the plugin,
         // return the pointer to the created plugin.
-        std::unique_ptr<PluginInterface, PluginDeleter<PluginInterface>> operator()(PluginServiceInterface& service);
-        std::shared_ptr<PluginInterface> operator()(PluginServiceInterface& service, AsSharedPtr /*create_shared*/);
+        PluginInterface* operator()(PluginServiceInterface& service);
         
     private:
         // function pointer for the createPlugin in our loaded plugin
         CreatePluginMethod createPlugin_;
-        DeletePluginMethod deletePlugin_;
-        
         LibraryHandle libraryHandle_;
     };
     
     
-    // initialize static member
     template<class PluginInterface, class PluginServiceInterface>
-    typename PosixPluginHandle<PluginInterface, PluginServiceInterface>::AsSharedPtr PosixPluginHandle<PluginInterface, PluginServiceInterface>::create_shared;
-    
-    template<class PluginInterface, class PluginServiceInterface>
-    PosixPluginHandle<PluginInterface, PluginServiceInterface>::PosixPluginHandle(LibraryHandle libraryHandle, void* createAddress, void* deleteAddress)
+    PosixPluginHandle<PluginInterface, PluginServiceInterface>::PosixPluginHandle(LibraryHandle libraryHandle, void* createAddress)
         : createPlugin_(reinterpret_cast<CreatePluginMethod>(createAddress))
-        , deletePlugin_(reinterpret_cast<DeletePluginMethod>(deleteAddress))
         , libraryHandle_(std::move(libraryHandle))
     {
     }
     
     template<class PluginInterface, class PluginServiceInterface>
-    std::unique_ptr<PluginInterface, PluginDeleter<PluginInterface>> PosixPluginHandle<PluginInterface, PluginServiceInterface>::operator()(PluginServiceInterface& service)
+    PluginInterface* PosixPluginHandle<PluginInterface, PluginServiceInterface>::operator()(PluginServiceInterface& service)
     {
-        auto plugin = createPlugin_(static_cast<void*>(&service));
-        return std::unique_ptr<PluginInterface, PluginDeleter<PluginInterface>>(static_cast<PluginInterface*>(plugin), PluginDeleter<PluginInterface>(deletePlugin_));
-    }
-    
-    template<class PluginInterface, class PluginServiceInterface>
-    std::shared_ptr<PluginInterface> PosixPluginHandle<PluginInterface, PluginServiceInterface>::operator()(PluginServiceInterface& service, AsSharedPtr /*create_shared*/)
-    {
-        auto plugin = createPlugin_(static_cast<void*>(&service));
-        return std::shared_ptr<PluginInterface>(static_cast<PluginInterface*>(plugin), PluginDeleter<PluginInterface>(deletePlugin_));
+        void* plugin = createPlugin_(static_cast<void*>(&service));
+        return static_cast<PluginInterface*>(plugin);
     }
 }}}
