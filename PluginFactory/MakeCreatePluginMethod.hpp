a28b6d7c518dc0d601bd20e53f64747943a08a1c
Austin Gilbert <ceretullis@gmail.com>
2015-10-21 16:22:05 -0500

    Renaming MAKE_PLUGIN_CREATION_METHOD -> MAKE_PLUGIN_METHODS.

--- PluginFactory/MakeCreatePluginMethod.hpp
+++ PluginFactory/MakeCreatePluginMethod.hpp
@@ -3,7 +3,7 @@
 // Use this helper macro to create a plugin creation method in your plugin implementation &
 // your base process. In the base process, the signature of the method will be used to lookup
 // the createPlugin() in the plugin shared library.
-#define MAKE_PLUGIN_CREATION_METHOD(PluginInterface, ConcretePluginImpl) \
+#define MAKE_PLUGIN_METHODS(PluginInterface, ConcretePluginImpl) \
 extern "C" { \
     void* createPlugin(void* service) \
     { \
