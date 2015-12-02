e6147ef19e6bab87c05e1c44acbd107a11d3db01
Austin Gilbert <ceretullis@gmail.com>
2015-12-02 12:29:12 -0600

    Adding DLL export commands to MakePluginMethods.

--- PluginFactory/MakePluginMethods.hpp
+++ PluginFactory/MakePluginMethods.hpp
@@ -1,10 +1,19 @@
-#pragma once 
+#pragma once
 #include <memory>
+
+#ifdef WIN32 
+	#define PLUGIN_FACTORY_DLL_EXPORT __declspec(dllexport)
+	#define PLUGIN_FACTORY_CDECL __cdecl 
+#else
+	#define PLUGIN_FACTORY_DLL_EXPORT 
+	#define PLUGIN_FACTORY_CDECL 
+#endif
+
 
 // Use this helper macro to create a plugin creation method in your plugin implementation.
 #define MAKE_PLUGIN_METHODS(PluginInterface, ConcretePluginImpl) \
 extern "C" { \
-    void* createPlugin(void* service) \
+    PLUGIN_FACTORY_DLL_EXPORT void* PLUGIN_FACTORY_CDECL createPlugin(void* service) \
     { \
         static std::unique_ptr<PluginInterface> plugin_(new ConcretePluginImpl(service)); \
         return plugin_.get(); \
